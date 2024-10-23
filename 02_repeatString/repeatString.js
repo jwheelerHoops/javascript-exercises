a6f1b9f6652af28cc7e797de3f53234197e948e9
jwheelerHoops <jwheeler1292@yahoo.com>
2024-10-23 10:37:00 -0700

    solution for exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let result = ""
+    if (num >= 0) {
+        for (i = 1; i <= num; i++) {
+            result = result + string
+        }
+    } else {
+        result = "ERROR"
+    }
 
+    return result
 };
 
 // Do not edit below this line
