0240b868c86b909f3011c3bb2bccd967c23510cc
jwheelerHoops <jwheeler1292@yahoo.com>
2024-10-24 10:46:20 -0700

    solution for exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(val1, val2) {
+	return val1 + val2
 };
 
-const subtract = function() {
-	
+const subtract = function(val1, val2) {
+	return val1 - val2
 };
 
-const sum = function() {
-	
+const sum = function(values) {
+  return values.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(values) {
+  return values.reduce((product, current) => product * current);
 };
 
-const power = function() {
-	
+const power = function(val1, val2) {
+	return Math.pow(val1, val2)
 };
 
-const factorial = function() {
-	
+const factorial = function(value) {
+  let solution = 1;
+
+  for (i = value; i > 0; i--) {
+    solution *= i;
+  }
+
+  return solution;
 };
 
 // Do not edit below this line
