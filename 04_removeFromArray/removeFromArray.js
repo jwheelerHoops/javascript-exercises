3c4dbc944a18beb8b169a69bc64ceac02991c277
jwheelerHoops <jwheeler1292@yahoo.com>
2024-10-24 09:01:26 -0700

    solution for exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(list, ...toRemove) {
+    let filteredList = list.filter(remove => !toRemove.includes(remove));
+
+    return filteredList;
 };
 
 // Do not edit below this line
