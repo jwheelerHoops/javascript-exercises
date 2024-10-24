df75d3e7a74a6dae44ece25f10b2cd744d4cff4b
jwheelerHoops <jwheeler1292@yahoo.com>
2024-10-24 11:07:11 -0700

    solution for exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let original = string.replace(/[!?,.\s]/g, "").toLowerCase();
+    let reversed = original.split("").toReversed().join("");
 
+    return original === reversed;
 };
 
 // Do not edit below this line
