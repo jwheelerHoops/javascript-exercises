09105705580310fabf6b54a865e628ed6e0c7d39
jwheelerHoops <jwheeler1292@yahoo.com>
2024-10-24 09:38:12 -0700

    solution for exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(inp1, inp2) {
+    let sum = 0;
 
+    if (inp1 < 0 || inp2 < 0) return "ERROR";
+    if (!Number.isInteger(inp1) || !Number.isInteger(inp2)) return "ERROR";
+    if (inp1 > inp2) [inp1, inp2] = [inp2, inp1];
+
+    for (i = inp1; i <= inp2; i++) {
+        sum += i;
+    };
+
+    return sum;
 };
 
 // Do not edit below this line
