2f14fd73dd29b0e29cd039aa5f5e1faed517e29a
jwheelerHoops <jwheeler1292@yahoo.com>
2024-10-24 11:51:56 -0700

    solution for exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
 
+    people.forEach(person => {
+        if (person.hasOwnProperty('yearOfDeath')) {
+            person['age'] = person.yearOfDeath - person.yearOfBirth
+        } else {
+            person['age'] = currentYear - person.yearOfBirth
+        };
+    });
+    people.sort((p1, p2) => (p1.age < p2.age) ? 1 : (p1.age > p2.age) ? -1 : 0)
+
+    return people[0];
 };
 
 // Do not edit below this line
