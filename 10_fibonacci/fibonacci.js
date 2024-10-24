63bff536aa145a6e842c2411860c6c972c88a5db
jwheelerHoops <jwheeler1292@yahoo.com>
2024-10-24 11:22:50 -0700

    solution for exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(value) {
+    let n = parseInt(value)
+    if (n < 0) return "OOPS";
 
+    let fibSeq = [0, 1];
+
+    for (i = 0; i < value; i++) {
+        fibSeq.push((fibSeq[i] + fibSeq[i + 1]))
+    }
+
+    return fibSeq[value]
 };
 
 // Do not edit below this line
